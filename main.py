4cfe078f83e56755e97e26f6c1843f61e087a609
Rahul <rahulyadav392@gmail.com>
2025-07-10 17:55:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Inventory App", layout="centered")
+st.title("📦 Inventory Management System")
+st.subheader("🚧 App under construction...")
+st.info("Connected to Streamlit and GitHub successfully.")
